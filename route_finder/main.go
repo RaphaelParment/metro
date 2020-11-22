d2655bda05822c412d83f4a0a788cbd3c1d230f3
Raphael Parment <raphael.parment@vestiairecollective.com>
2020-11-22 15:24:48 +0100

    Remove binaries.

--- route_finder/main.go
+++ route_finder/main.go
@@ -8,56 +8,7 @@ import (
 	"os"
 )
 
-func loadStraightLine(network *metro.Network, number int) (*metro.Line, error) {
-	fileName := fmt.Sprintf("data/metro_%d.txt", number)
-	f, err := os.Open(fileName)
-	if err != nil {
-		log.Printf("failed to open file %q; %v", fileName, err)
-		return nil, err
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			log.Printf("failed to close %q; %v", fileName, err)
-		}
-	}()
 
-	line := metro.NewLine(fmt.Sprint(number), number, nil)
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-
-	var stations []*metro.Station
-	counter := 0
-	var current *metro.Station
-
-	for scanner.Scan() {
-		var station *metro.Station
-		name := scanner.Text()
-
-		if s, ok := network.Stations[name]; !ok {
-			station = metro.NewStation(name, number)
-			network.Stations[name] = station
-		} else {
-			station = s
-		}
-		stations = append(stations, station)
-
-		if counter > 0 {
-			stations[counter].Neighbours = append(station.Neighbours, current)
-			stations[counter].DistToNeighbours[current] = 1
-		}
-
-		current = station
-		if counter > 0 {
-			stations[counter-1].Neighbours = append(stations[counter-1].Neighbours, current)
-			stations[counter-1].DistToNeighbours[current] = 1
-		}
-		counter++
-		line.Stations = append(line.Stations, station)
-	}
-
-	return line, nil
-}
 
 func run() error {
 	network := metro.NewNetwork()
